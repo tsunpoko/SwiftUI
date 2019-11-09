c7651b32984e96d4f33ca8862aefefea519f3cdb
Takato Nakamatsu <tsun@TakatonoMacBook-Air.local>
2019-11-09 14:23:42 +0900

    initialize

---
+++ testapp/testapp/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  testapp
+//
+//  Created by Takato Nakamatsu on 2019/11/09.
+//  Copyright © 2019 Takato Nakamatsu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+                .foregroundColor(.blue)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
