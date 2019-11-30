7f58da44c1b5c3ad1e91a75313a7738474f4181b
Takato Nakamatsu <tsun@TakatonoAir.lan>
2019-11-30 15:16:58 +0900

    initialized githubApp

---
+++ githubApp/githubApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  githubApp
+//
+//  Created by Takato Nakamatsu on 2019/11/30.
+//  Copyright © 2019 Takato Nakamatsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
